bb58da3e5980f38c5b1771f62394e2c1fcf07504
Márcio Gusmão <marciogusmao@msn.com>
2021-06-06 19:06:21 -0300

    Update MediaEscolarCondicional.js

--- MediaEscolarCondicional.js
+++ MediaEscolarCondicional.js
@@ -1,27 +1,32 @@
+const prompt = require('prompt-sync')()
+
 console.log('####################################')
 console.log('####### Média do Aluno 2021! #######')
 console.log('####################################')
 
+console.log('\n')
+
 let dados = true
-while (dados = prompt('\nDeseja calcular a média do aluno? (s) para Sim (n) para Não: ')) {
+while (dados = prompt('Deseja calcular a média do aluno? (s) para Sim (n) para Não: ')) {
   if (dados !== 's' && dados !== 'n') {
-    console.log('\nEntrada Invalida!')
+    console.log('\n' + 'Entrada Invalida!')
   }
   else if (dados === 's') {
-    let nome = prompt(`\nQual o nome do aluno? `)
-    let n1 = parseFloat(prompt(`\nQual a primeira nota? `))
+    console.log('\n')
+    let nome = prompt(`Qual o nome do aluno? `)
+    let n1 = parseFloat(prompt(`Qual a primeira nota? `))
     let n2 = parseFloat(prompt(`Qual a segunda nota? `))
     let n3 = parseFloat(prompt(`Qual a terceira nota? `))
     let n4 = parseFloat(prompt(`Qual a quarta nota? `))
     let media = (n1 + n2 + n3 + n4) / 4
 
-    console.log(`\nA média do aluno ${nome} é ${media}`)
+    console.log(`\n` + `A média do aluno ${nome} é ${media}`)
 
     if (media >= 7){
-      console.log(`\nAluno ${nome} aprovado!`)
+      console.log(`\n` + `Aluno ${nome} aprovado!` + `\n`)
     }
     else {
-      console.log(`\nAluno ${nome} reprovado!`)
+      console.log(`\n` + `Aluno ${nome} reprovado!` + `\n`)
     }
   }
     else {
